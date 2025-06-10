9b163fee10200b92f20ec31a1620af0b700ff878
David Lazic <lazic.ddavid@gmail.com>
2025-06-10 19:29:00 +0200

    added comment

---
+++ RollDice.js
@@ -0,0 +1,6 @@
+// roll dice:
+// napravi array slike
+// u taj array sacuvaj 6 slika ("./slika1.jpg")
+// tako da je, slike[0 ] = slika1.jpg
+// kad kliknem roll , izgenerisem random broj
+// taj random broj iskoristim da pogodim sliku iz array slike
