cceeac4f5c413fa9c3b5fca76cbb140c86a07fcf
David Lazic <lazic.ddavid@gmail.com>
2025-06-11 18:55:29 +0200

    added function

--- RollDice.js
+++ RollDice.js
@@ -4,4 +4,13 @@
 // tako da je, slike[0 ] = slika1.jpg
 // kad kliknem roll , izgenerisem random broj
 
-const slike = ["./slika1.jpg"];
+function prikaziRandomSliku() {
+  const slike = [
+    "./slika1.jpg",
+    "./slika2.jpg",
+    "./slika3.jpg",
+    "./slika4.jpg",
+    "./slika5.jpg",
+    "./slika6.jpg",
+  ];
+}
