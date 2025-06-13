d52922c2b9954bec1d7066b403df116ea128a0ac
David Lazic <lazic.ddavid@gmail.com>
2025-06-13 22:34:15 +0200

    added addEventListener

--- RollDice.js
+++ RollDice.js
@@ -25,7 +25,7 @@ taster.addEventListener("click", prikaziRandomSliku);
 
 function ()
 
-
+addEventListener ()
 //napravi jednu varijablu koja se zove totalscore
 // pocinje sa nulom
 // i svaki roll dodaje na taj broj izrolani broj
