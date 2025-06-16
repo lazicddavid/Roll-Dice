ae3c6705a2f71d91bc5ab3a991d9027a4fe541e8
David Lazic <lazic.ddavid@gmail.com>
2025-06-16 20:56:23 +0200

    added totalScore

--- RollDice.js
+++ RollDice.js
@@ -3,11 +3,33 @@
 // u taj array sacuvaj 6 slika ("./slika1.jpg")
 // tako da je, slike[0 ] = slika1.jpg
 // kad kliknem roll, izgenerisem random broj
+console.log("------------------");
+
+//napravi jednu varijablu koja se zove totalscore
+// pocinje sa nulom
+// i svaki roll dodaje na taj broj izrolani broj
+// svaki put ispisi vrednost na ekranu
+
+//ako se dobije jedinica, skor se restartuje na nulu
+// napravi dugme restart
+//kad se ono klikne, igra se restartuje
+
+//pokusaj da:
+// u listi ispises svaki izrolani broj
+// napravi u html prazni ul
+// nakon svakog izrolanog broja u JS napravi list element
+
+// unutar li elementa, da bude paragraf sa tekstom: "izrolani broj je: neki broj, totalScoreje:
+// taj list element dodaj u listu
+//trebace ti funkcije: createElement i appendChild. Moze se iskoristiti innerHTML
+//
+//obrati paznju da ovo isto uradis i na Restart
 
 const taster = document.getElementById("roll");
 const slika = document.getElementById("prikaz");
 const rezultat = document.getElementById("rezultat");
 const restartBtn = document.getElementById("restart");
+const lista = document.getElementById("lista-brojeva");
 
 let totalscore = 0;
 
@@ -33,6 +55,12 @@ function prikaziRandomSliku() {
   }
 
   rezultat.textContent = `Ukupan skor: ${totalscore}`;
+
+  const noviLi = document.createElement("li");
+  const paragraf = document.createElement("p");
+  paragraf.textContent = `Izrolani broj je: ${broj}, totalScore je: ${totalscore}`;
+  noviLi.appendChild(paragraf);
+  lista.appendChild(noviLi);
 }
 
 taster.addEventListener("click", prikaziRandomSliku);
@@ -41,23 +69,5 @@ restartBtn.addEventListener("click", function () {
   totalscore = 0;
   slika.src = "";
   rezultat.textContent = "Ukupan skor: 0";
+  lista.innerHTML = "";
 });
-
-//napravi jednu varijablu koja se zove totalscore
-// pocinje sa nulom
-// i svaki roll dodaje na taj broj izrolani broj
-// svaki put ispisi vrednost na ekranu
-
-//ako se dobije jedinica, skor se restartuje na nulu
-// napravi dugme restart
-//kad se ono klikne, igra se restartuje
-
-//pokusaj da:
-// u listi ispises svaki izrolani broj
-// napravi u html prazni ul
-// nakon svakog izrolanog broja u JS napravi list element
-// unutar li elementa, da bude paragraf sa tekstom: "izrolani broj je: neki broj, totalScoreje:
-// taj list element dodaj u listu
-//trebace ti funkcije: createElement i appendChild. Moze se iskoristiti innerHTML
-//
-//obrati paznju da ovo isto uradis i na Restart
